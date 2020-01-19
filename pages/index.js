5ca740a0e0b7961319e42af3d9c6a8c4073ec70f
Radu F Dunarentu <dunarentu.radu@gmail.com>
2020-01-19 22:25:08 +0200

    show a list of mocked products

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,28 @@
 import React from 'react'
-import Link from 'next/link'
+import fetch from 'isomorphic-unfetch';
 
-export default () => {
-  return(<div>home</div>);
-}
+const App = ({products}) => {
+  return(
+    <div>
+      <h1>Online Vending machine</h1>
+      <ul>
+        {products.length && products.map(product => (
+          <li key={product.id}>
+              <span>{product.name} A: {product.quantity} Price:{product.price} $</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+App.getInitialProps = async () => {
+  const res = await fetch('http://localhost:3000/api/products');
+  const {data: products} = await res.json();
+  console.log('get products', products);
+  return {
+    products
+  }
+};
+
+export default App;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,54 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/mock', (req, res) => {
-  res.json({x: 'cici are mere'});
+router.get('/products', (req, res) => {
+  res.json({data: [{
+      id: 1,
+      quantity: 10,
+      name: 'product 1',
+      price: 30
+    },
+    {
+      id: 2,
+      quantity: 10,
+      name: 'product 2',
+      price: 35
+    },
+    {
+      id: 3,
+      quantity: 10,
+      name: 'product 3',
+      price: 25
+    },
+    {
+      id: 4,
+      quantity: 10,
+      name: 'product 4',
+      price: 15
+    },
+    {
+      id: 5,
+      quantity: 10,
+      name: 'product 5',
+      price: 5
+    },
+    {
+      id: 6,
+      quantity: 10,
+      name: 'product 6',
+      price: 45
+    },
+    {
+      id: 7,
+      quantity: 10,
+      name: 'product 7',
+      price: 55
+    },
+    {
+      id: 8,
+      quantity: 10,
+      name: 'product 8',
+      price: 35
+    }]});
 });
 module.exports = router;
