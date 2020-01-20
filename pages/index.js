bc4401cf14ac92cda67dfd35c65d33a1a32886e8
Radu F Dunarentu <dunarentu.radu@gmail.com>
2020-01-21 00:27:01 +0200

    add but products in server memory

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,19 @@ import fetch from 'isomorphic-unfetch';
 
 // todo: make them all strings
 const keyPadInputs = [1,2,3,4,5,6,7,8,9,'C',0,'B'];
-const App = ({products}) => {
+const productCodes = ['00', '01', '02','03', '04', '05', '06', '07', '08', '09', '10', '11', '12','13', '14', '15','16'];
+const mapCodesToProducts = (a, index) => ({code: productCodes[index], ...a});
+const coins = [1,3,5,10,20];
+const App = (props) => {
 
+  const [products, setProducts] = useState(props.products);
   const [input, setInput] = useState('');
   const [error, setError] = useState('');
   // todo: add text in variables
   const [info, setInfo] = useState('Please select a product by code');
+  const [balance, setBalance] = useState(0);
+  // used for temp input sum
+  const [sum, setSum] = useState(0);
 
   const handleKeyPad = (keypadInput) => {
     //todo: nice to have a limit
@@ -25,11 +32,17 @@ const App = ({products}) => {
     }
   };
 
-  const buyProduct = () => {
-    console.log('code is', input);
+  const buyProduct = async () => {
     const product = products.find(c => c.code === input);
     if (product) {
       console.log('try to buy product', product);
+      try {
+        const res = await fetch(`http://localhost:3000/api/products/${product.id}`, { method: 'POST'});
+        const {data} = await res.json();
+        setProducts(data.map(mapCodesToProducts));
+      } catch(e) {
+        setError(e.error);
+      }
       // do a post request with the product id
     } else {
       setError('Code invalid');
@@ -39,7 +52,8 @@ const App = ({products}) => {
   };
 
   return(
-    <div>
+    <div className='container'>
+      <div>
       <h1>Online Vending machine</h1>
       <div className='products-container'>
         {products.length && products.map(product => (
@@ -52,16 +66,39 @@ const App = ({products}) => {
           </div>
         ))}
       </div>
-      <h2>Keypad to select product</h2>
-      <div className='keypad-container'>
-        {keyPadInputs.map(input => (<span key={input} className='keypad-el' onClick={() => handleKeyPad(input)}>{input}</span>))}
+        </div>
+      <div>
+        <h2>Keypad to select product</h2>
+        <div className='keypad-container'>
+          {keyPadInputs.map(input => (<span key={input} className='keypad-el' onClick={() => handleKeyPad(input)}>{input}</span>))}
+        </div>
+        <h3>Keypad Input</h3>
+        <div>{input ? input : info}</div>
+        <div><button onClick={buyProduct}>Select</button></div>
+        {error && <div>{error}</div>}
+        <div>Balance: {balance ? balance : '0 Please Add Money'}</div>
+        <div style={{display: 'flex', alignItems: 'baseline'}}>Insert Money {coins.map(c => (<span key={c} className='coin' onClick={() => setBalance(balance + c)}>{c}</span>))} </div>
       </div>
-      <h3>Keypad Input</h3>
-      <div>{input ? input : info}</div>
-      <div><button onClick={buyProduct}>Select</button></div>
-      {error && <div>{error}</div>}
+
 
       <style jsx>{`
+      .container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+      }
+      .coin {
+        cursor: pointer;
+        background-color: gold;
+        border: solid black;
+        border-radius: 69px;
+        width: 19px;
+        margin: 2px;
+        padding: 2px;
+        display: flex;
+        justify-content: center;
+      }
       .products-container {
         display: grid;
         grid-template-columns: repeat(4, 100px);
@@ -105,14 +142,15 @@ const App = ({products}) => {
   );
 };
 
-const productCodes = ['00', '01', '02','03', '04', '05', '06', '07', '08', '09', '10', '11', '12','13', '14', '15','16'];
+
 App.getInitialProps = async () => {
   const res = await fetch('http://localhost:3000/api/products');
   const {data} = await res.json();
   console.log('get products', data);
   return {
-    products: data.map((a, index) => ({code: productCodes[index], ...a}))
+    products: data.map(mapCodesToProducts)
   }
 };
+
 
 export default App;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,54 +1,68 @@
 const express = require('express');
 const router = express.Router();
 
+const products = [{
+  id: '1',
+  quantity: 10,
+  name: 'product 1',
+  price: 30
+},
+  {
+    id: '2',
+    quantity: 10,
+    name: 'product 2',
+    price: 35
+  },
+  {
+    id: '3',
+    quantity: 10,
+    name: 'product 3',
+    price: 25
+  },
+  {
+    id: '4',
+    quantity: 10,
+    name: 'product 4',
+    price: 15
+  },
+  {
+    id: '5',
+    quantity: 10,
+    name: 'product 5',
+    price: 5
+  },
+  {
+    id: '6',
+    quantity: 10,
+    name: 'product 6',
+    price: 45
+  },
+  {
+    id: '7',
+    quantity: 10,
+    name: 'product 7',
+    price: 55
+  },
+  {
+    id: '8',
+    quantity: 10,
+    name: 'product 8',
+    price: 35
+  }];
+
 router.get('/products', (req, res) => {
-  res.json({data: [{
-      id: 1,
-      quantity: 10,
-      name: 'product 1',
-      price: 30
-    },
-    {
-      id: 2,
-      quantity: 10,
-      name: 'product 2',
-      price: 35
-    },
-    {
-      id: 3,
-      quantity: 10,
-      name: 'product 3',
-      price: 25
-    },
-    {
-      id: 4,
-      quantity: 10,
-      name: 'product 4',
-      price: 15
-    },
-    {
-      id: 5,
-      quantity: 10,
-      name: 'product 5',
-      price: 5
-    },
-    {
-      id: 6,
-      quantity: 10,
-      name: 'product 6',
-      price: 45
-    },
-    {
-      id: 7,
-      quantity: 10,
-      name: 'product 7',
-      price: 55
-    },
-    {
-      id: 8,
-      quantity: 10,
-      name: 'product 8',
-      price: 35
-    }]});
+  res.json({data: products});
+});
+
+router.post('/products/:id', (req, res) => {
+  // do some in memory changes
+  const product = products.find(p => p.id === req.params.id);
+  if (product && product.quantity) {
+    product.quantity -=1;
+    res.json({data: products});
+  } else {
+    res.status(406).send({err: 'no more products left'})
+  }
+
 });
 module.exports = router;
